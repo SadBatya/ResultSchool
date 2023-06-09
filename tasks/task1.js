6e4e7d0396a7ff5f975a918d63d31383121c792b
sadbatya <sadbatya@mail.ru>
2023-06-09 08:49:58 +0200

    task1

---
+++ tasks/task1.js
@@ -0,0 +1,20 @@
+const peopleWaiting = ['Кристина', 'Олег', 'Кирилл', 'Мария', 'Светлана', 'Артем', 'Глеб'];
+
+//Удаляем первых двех человек, которые получили посылку
+const firstPeopleWhoReceivedPackage = peopleWaiting.shift()
+const secondPeopleWhoReceivedPackage = peopleWaiting.shift()
+
+
+//Выводим кто получил посылку и проверяем сколько человек осталось в очереди
+console.log(`${firstPeopleWhoReceivedPackage} и ${secondPeopleWhoReceivedPackage} получили посылку`)
+console.log(`В очереди ${peopleWaiting.length} человек`)
+
+//Обед, поэтому убираем людей, которые стояли в очереди
+const firstPeopleWhoDidNotReceivePackage = peopleWaiting.shift(1)
+const otherPeople = peopleWaiting.splice(0).toString()
+
+//Считаем сколько человек не получили посылку
+console.log(`${firstPeopleWhoDidNotReceivePackage} и ${otherPeople} не получили посылку`)
+console.log(`В очереди ${peopleWaiting.length} человек`)
+
+

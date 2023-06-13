f05b94d56d151df28710bdfe4b2a75222b6f5842
sadbatya <sadbatya@mail.ru>
2023-06-13 21:39:18 +0200

    push

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,3 +1,18 @@
+// addDays = (date, days) => {
+//  let dateNow = Number(date)
+//  let dateNow2 = new Date()
+//  let dateNow3 = (`${dateNow2.getFullYear()} .${dateNow2.getDay()} . ${dateNow2.getMonth()}`)
+
+//  console.log(date)
+//  console.log(dateNow2)
+//  console.log(dateNow3)
+//  console.log(typeof dateNow)
+// }
+
 addDays = (date, days) => {
- date = new Date(Date.now() + (days * 24 * 60 * 3600))
+  let newDate = Date.now(new Date(date))
+  let futureDate = newDate + 
+  console.log(newDate)
 }
+
+addDays('05.05.2021', 1)

e37a6814e32293172603b6ebb0c8c7be00daac16
sadbatya <sadbatya@mail.ru>
2023-06-25 14:38:14 +0200

    done

--- tasks/task5.js
+++ tasks/task5.js
@@ -1,29 +1,67 @@
 class Developer{
   constructor(fullName, age, position){
-    this.fullName = fullName;
-    this.age = age;
-    this.position = position;
-    let technologies = [];
+    this.fullName = fullName
+    this.age = age
+    this.position = position
+    let technologies = []
   }
 
   code(){}
-
   learnNewTechnologies(technology){
     this.technologies.push(technology)
   }
 }
 
 class JuniorDeveloper extends Developer{
-  constructor(fullname, age, position){
-    super(fullname, age)
-    this.position = position
+  constructor(fullName, age){
+    super(fullName, age, 'Junior')
+    this.technologies = ['HTML', 'CSS', 'JavaScript']
+  }
+
+  code(){
+    console.log('Junior разработчик пишет код...')
   }
   
+}
+
+class MiddleDeveloper extends Developer{
+  constructor(fullName, age){
+    super(fullName, age, 'Middle')
+    this.technologies = ['HTML', 'CSS', 'JavaScript', 'React']
+  }
+
   code(){
-    console.log(`${this.position} разработчик пишет код ${codelearnNewTechnologies('HTML', 'CSS', 'JavaScript')}`)
+    console.log('Middle разработчик пишет код...')
   }
 }
 
-const junior = new JuniorDeveloper('Vladimir', 25, 'Junior')
-console.log(junior)
-console.log(junior.code())
+class SeniorDeveloper extends Developer{
+  constructor(fullName, age){
+    super(fullName, age, 'Middle')
+    this.technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS']
+  }
+
+  code(){
+    console.log('Senior разработчик пишет код...')
+  }
+}
+
+
+const juniorDeveloper = new JuniorDeveloper('Анастасия', 20);
+const middleDeveloper = new MiddleDeveloper('Игорь', 25);
+const seniorDeveloper = new SeniorDeveloper('Максим', 30);
+
+juniorDeveloper.code(); // Junior разработчик пишет код...
+middleDeveloper.code(); // Middle разработчик пишет код...
+seniorDeveloper.code(); // Senior разработчик пишет код...
+
+seniorDeveloper.learnNewTechnologies('Docker');
+
+console.log(juniorDeveloper.fullName, juniorDeveloper.age, juniorDeveloper.position, juniorDeveloper.technologies); 
+// 'Анастасия' 20 'Junior' ['HTML', 'CSS', 'JavaScript']
+
+console.log(middleDeveloper.fullName, middleDeveloper.age, middleDeveloper.position, middleDeveloper.technologies); 
+// 'Игорь' 25 'Middle' ['HTML', 'CSS', 'JavaScript', 'React']
+
+console.log(seniorDeveloper.fullName, seniorDeveloper.age, seniorDeveloper.position, seniorDeveloper.technologies); 
+// 'Максим' 30 'Senior' ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS', 'Docker']

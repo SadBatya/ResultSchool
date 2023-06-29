1b4309d651fa1272cc34b471f8fba1f37ba1734a
sadbatya <sadbatya@mail.ru>
2023-06-29 13:43:18 +0200

    task2

--- tasks/task2/index.js
+++ tasks/task2/index.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('.create-task-block')
 const taskList = document.querySelector('.tasks-list')
-const taskText = document.querySelectorAll('task')
+const taskText = document.querySelectorAll('.task')
+
 
 
 
@@ -9,8 +10,10 @@ form.addEventListener('submit', (event) => {
   const { target } = event
   const taskNameInput = target.taskName
   const inputValue = taskNameInput.value
+  const span = document.createElement('span')
 
 
+ 
   if(inputValue){
     const divTask = document.createElement('div')
     divTask.className = 'task'
@@ -18,12 +21,13 @@ form.addEventListener('submit', (event) => {
     divTask.innerHTML = inputValue
     taskList.append(divTask)
   }else if(!inputValue){
-    const span = document.createElement('span')
     span.className = 'error-message-block'
     span.innerHTML = 'Название задачи не должно быть пустым'
-    
+    form.append(span)
   }
-
-
 })
+
+
+
+
 

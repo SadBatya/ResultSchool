dc4f97206a23687952985f89a517bff80086d941
sadbatya <sadbatya@mail.ru>
2023-09-15 14:51:50 +0200

    calculator done

--- reactmodule/src/MyComponent.jsx
+++ reactmodule/src/MyComponent.jsx
@@ -6,21 +6,23 @@ const numbers = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
 export default function MyComponent() {
 	const [value, setValue] = useState('');
-	console.log(typeof numbers)
+	// const handleClick = (sym) => {
+	// 	setValue((updateValue) => updateValue + '+')
+	// }
 	return (
 		<div className={styles.calculator}>
 			<div className={styles.calculator__inner}>
 				<input className={styles.input} type="text" value={value} />
 				<div className={styles.buttons}>
 					{numbers.map((num) => (
-						<button onClick={() => setValue((updateValue) => updateValue + {num})} className={styles.symbols}>{num}</button>
+						<button onClick={() => setValue((updateValue) => updateValue + num)} className={styles.symbols}>{num}</button>
 					))}
 					<button onClick={() => setValue((updateValue) => updateValue + '+')} className={styles.symbols}>+</button>
 					<button onClick={() => setValue((updateValue) => updateValue + '-')} className={styles.symbols}>-</button>
-					<button onClick={() => setValue((updateValue) => updateValue + '=')} className={styles.symbols}>=</button>
-					<button onClick={() => setValue((updateValue) => updateValue + '=')} className={styles.symbols}>C</button>
+					<button onClick={() => setValue((updateValue) => eval(updateValue))} className={styles.symbols}>=</button>
+					<button onClick={() => setValue(() => setValue(''))} className={styles.symbols}>C</button>
 				</div>
-				<p>{value}</p>
+				
 			</div>
 		</div>
 	);

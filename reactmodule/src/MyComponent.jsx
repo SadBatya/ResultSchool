2ee67d8d10f56633e43e202afa7c4dac6a5a0c60
sadbatya <sadbatya@mail.ru>
2023-09-15 15:02:12 +0200

    fix

--- reactmodule/src/MyComponent.jsx
+++ reactmodule/src/MyComponent.jsx
@@ -12,17 +12,42 @@ export default function MyComponent() {
 	return (
 		<div className={styles.calculator}>
 			<div className={styles.calculator__inner}>
+				<h1>calculator v1.0</h1>
 				<input className={styles.input} type="text" value={value} />
 				<div className={styles.buttons}>
 					{numbers.map((num) => (
-						<button onClick={() => setValue((updateValue) => updateValue + num)} className={styles.symbols}>{num}</button>
+						<button
+							onClick={() => setValue((updateValue) => updateValue + num)}
+							className={styles.symbols}
+						>
+							{num}
+						</button>
 					))}
-					<button onClick={() => setValue((updateValue) => updateValue + '+')} className={styles.symbols}>+</button>
-					<button onClick={() => setValue((updateValue) => updateValue + '-')} className={styles.symbols}>-</button>
-					<button onClick={() => setValue((updateValue) => eval(updateValue))} className={styles.symbols}>=</button>
-					<button onClick={() => setValue(() => setValue(''))} className={styles.symbols}>C</button>
+					<button
+						onClick={() => setValue((updateValue) => updateValue + '+')}
+						className={styles.symbols}
+					>
+						+
+					</button>
+					<button
+						onClick={() => setValue((updateValue) => updateValue + '-')}
+						className={styles.symbols}
+					>
+						-
+					</button>
+					<button
+						onClick={() => setValue((updateValue) => eval(updateValue))}
+						className={styles.symbols}
+					>
+						=
+					</button>
+					<button
+						onClick={() => setValue(() => setValue(''))}
+						className={styles.symbols}
+					>
+						C
+					</button>
 				</div>
-				
 			</div>
 		</div>
 	);
